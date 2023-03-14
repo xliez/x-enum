832a364a9ae0e3b89ae4062290444ac13f17145d
xliez <xliez@foxmail.com>
2023-03-14 13:30:11 +0800

    feat: dev

--- README.md
+++ README.md
@@ -8,57 +8,50 @@
 
 有以下这些使用场景：
 
-1. select 组件的options: 一般为 `{ label: string; value: string | number }[]` 这样的数据
+1. select 组件的 options: 一般为 `{ label: string; value: string | number }[]` 这样的数据
 2. 根据 key 获取 value
 3. 根据 key 获取 label
 4. 根据 value 获取 label
 5. 根据 value 获取 key
 
-该函数工具封装了以上业务场景的方法，方便维护枚举值
+该函数工具封装了以上业务场景的方法，方便维护枚举值，并且**TS 支持 key 值的枚举推断**
 
-### 使用方式
+## 使用方式
 
 ```ts
-import { Select } from 'antd';
-import { EnumTool } from '@xliez/enum-tool';
+import { Select } from "antd";
+import { xEnum } from "@xliez/enum-tool";
 
-const TypeEnum = new EnumTool([
-    ['TODO', 0, '待办'],
-    ['PENDING', 1, '处理中'],
-    ['DONE', 2, '已完成']
-] as const)
+const TypeEnum = xEnum([
+  ["TODO", 0, "待办"],
+  ["PENDING", 1, "处理中"],
+  ["DONE", 2, "已完成"],
+]);
 
 // 1. 生成 select 组件 options
 const App = () => {
   return (
     <>
-      <Select label="select" name="select" options={genOptions()} />
+      <Select label="select" name="select" options={TypeEnum.genOptions()} />
     </>
   );
 };
 
 // 2. 根据 key 取 value
-const value = TypeEnum.valueByKey('TODO')
+const value = TypeEnum.TODO.value; // 支持TS推断
+// or
+const value = TypeEnum.valueByKey("TODO");
+
+// 3. 根据 key 取 label
+const label = TypeEnum.TODO.label; // 支持TS推断
+// or
+const label = TypeEnum.labelByKey("TODO");
+
+// 4. 根据 value 取 label
+const label = TypeEnum.labelByValue(0);
+
+// 5. 根据 value 取 key
+const key = TypeEnum.keyByValue(0);
 
 export default App;
-```
-
-### 定义
-
-```ts
-function enumTool(origin: Record<string, EnumValue>): {
-  // 获取原始对象
-  origin: Record<string, EnumValue>;
-  // 根据 key 获取 value
-  keyToVal: (key: string) => EnumValue;
-  // 根据 value 获取 key
-  valToKey: (value: EnumValue) => EnumValue;
-  /* 生成 Select 组件的 options
-   * names 为生成options数组中代表 label 和 value 的名称，默认为 ['label', 'value']
-   * 如 genOptions(['name', 'id']) => 生成 [{ name: 'A', id: 'a' }, { name: 'B', id: 'b' }, { name: 'C', id: 'c' }]
-   */
-  genOptions: (names?: [string, string]) => {
-    [labelName: string]: EnumValue;
-  }[];
-};
 ```
--- src/enum-tool.ts
+++ src/enum-tool.ts
@@ -1,23 +1,18 @@
 type EnumValue = string | number;
-type KVL = [string, EnumValue, string?];
+type KVL<T extends string> = [T, EnumValue, string?];
+type VLObj = { value: EnumValue; label?: string };
 
 /**
  * 枚举工具类
  */
-export class EnumTool {
-  private kvMap: Record<
-    KVL[0],
-    {
-      value: KVL[1];
-      label: KVL[2];
-    }
-  > = {};
+export class EnumTool<T extends string> {
+  private kvMap: { [key in T]: VLObj } = {} as any;
 
   private optionNames: [string, string] = ["label", "value"];
 
   private optionsCache: null | { [name: string]: EnumValue }[] = null;
 
-  constructor(kvls: Readonly<Readonly<KVL>[]>) {
+  constructor(kvls: KVL<T>[]) {
     kvls.forEach(([key, value, label]) => {
       this.kvMap[key] = {
         value,
@@ -26,20 +21,23 @@ export class EnumTool {
     });
   }
 
-  public valueByKey(key: string) {
+  public valueByKey(key: T) {
     return this.kvMap[key].value;
   }
 
-  public lableByKey(key: string) {
+  public lableByKey(key: T) {
     return this.kvMap[key].label;
   }
 
   public labelByValue(value: EnumValue) {
-    return Object.values(this.kvMap).find(item => item.value === value)?.label;
+    return Object.values<VLObj>(this.kvMap).find(item => item.value === value)
+      ?.label;
   }
 
   public keyByValue(value: EnumValue) {
-    return Object.keys(this.kvMap).find(key => this.kvMap[key].value === value);
+    return Object.keys(this.kvMap).find(
+      key => this.kvMap[key as T].value === value
+    );
   }
 
   public genOptions(names?: [string, string]) {
@@ -51,7 +49,7 @@ export class EnumTool {
     ) {
       return this.optionsCache;
     }
-    this.optionsCache = Object.entries(this.kvMap).map(
+    this.optionsCache = Object.entries<VLObj>(this.kvMap).map(
       ([key, { value, label }]) => ({
         [labelName]: label ?? key,
         [valueName]: value,
@@ -66,3 +64,15 @@ export class EnumTool {
     return this.kvMap;
   }
 }
+
+export const xEnum = <T extends string>(kvls: KVL<T>[]) => {
+  const e = new EnumTool(kvls);
+  return {
+    ...e.getMap(),
+    valueByKey: e.valueByKey.bind(e),
+    lableByKey: e.lableByKey.bind(e),
+    labelByValue: e.labelByValue.bind(e),
+    keyByValue: e.keyByValue.bind(e),
+    genOptions: e.genOptions.bind(e),
+  };
+};
